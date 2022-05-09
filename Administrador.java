213cdefeda281d0d88dc92f56d8925e8fbf190e7
CabreraLucas <lucas.cabrera@fatec.sp.gov.br>
2022-05-09 19:52:55 +0000

    Exercicio1

--- Administrador.java
+++ Administrador.java
@@ -1,3 +1,17 @@
-public class Administrador {
-    
+public class Administrador extends Empregado {
+
+    private double ajudaCusto;
+
+    public double getAjudaCusto() {
+        return ajudaCusto;
+    }
+    public void setAjudaCusto(double ajudaCusto) {
+        this.ajudaCusto = ajudaCusto;
+    }
+
+    public void calcularAaaa(){
+        double salarioADM = calcularSalario() + ajudaCusto;
+
+    }
+
 }
